83d79d2c6c315300b154f5ab6c5136c2008687e1
Tomas Trajan <tomas.trajan@gmail.com>
2016-01-27 23:13:25 +0100

    feat(tests): add more tests, adjust test description

--- test/es5-es6.js
+++ test/es5-es6.js
@@ -1,32 +1,32 @@
+"use strict";
 
 var assert = require("assert");
 var es6 = require('../lib/module-es6');
 
-describe("ES5 module importing ES6 module", function() {
+describe("ES5 module importing ES6 module", () => {
 
-    it("imports ES6 module and checks exported function", function() {
+    it("checks exported function", () => {
         assert.equal(es6.testFunction(), "testFunction");
     });
 
-    it("imports ES6 module and checks exported const", function() {
+    it("checks exported const", () => {
         assert.equal(es6.testConst, "testConst");
     });
 
-    it("imports ES6 module and checks exported let", function() {
+    it("checks exported let", () => {
         assert.equal(es6.testLet, "testLet");
     });
 
-    it("imports ES6 module and checks exported var", function() {
+    it("checks exported var", () => {
         assert.equal(es6.testVar, "testVar");
     });
 
-    it("imports ES6 module and checks exported class", function() {
+    it("checks exported class", () => {
         assert.equal(new es6.TestClass().testProp, "testProp");
     });
 
-    it("imports ES6 module and checks default export", function() {
+    it("checks default export - only explicit \"default\" call, by default NOT backward compatible", () => {
         assert.equal(es6.default(), "defaultTestFunction");
-
     });
 
 });
--- test/es6-es5.js
+++ test/es6-es5.js
@@ -1,61 +1,60 @@
-
 import assert from "assert";
 
-import es5 from '../lib/module-es5';
-import * as es5star from '../lib/module-es5';
-import { testFunction, testVar, testObject } from '../lib/module-es5';
+import es5 from "../lib/module-es5";
+import * as es5star from "../lib/module-es5";
+import { testFunction, testVar, testObject } from "../lib/module-es5";
 
-import es5exports from '../lib/module-es5-exports';
-import * as es5starExports from '../lib/module-es5-exports';
-import { testProp, testMethod } from '../lib/module-es5-exports';
+import es5exports from "../lib/module-es5-exports";
+import * as es5starExports from "../lib/module-es5-exports";
+import { testProp, testMethod } from "../lib/module-es5-exports";
 
-describe("ES6 module importing ES5 module with exports. syntax", function() {
+describe("ES6 module importing ES5 module with \"exports.\" syntax", () => {
 
-    describe("ES6 module importing ES5 module as default import", function() {
+    describe("import es5 from \"../lib/module-es5\";", () => {
 
-        it("imports ES5 module and checks exported function", function() {
+        it("checks exported function", () => {
             assert.equal(es5.testFunction(), "testFunction");
         });
 
-        it("imports ES5 module and checks exported var", function() {
+        it("checks exported var", () => {
             assert.equal(es5.testVar, "testVar");
         });
 
-        it("imports ES5 module and checks exported object", function() {
+        it("checks exported object", () => {
             assert.equal(es5.testObject.testProp, "testProp");
             assert.equal(es5.testObject.testMethod(), "testMethod");
         });
 
     });
 
-    describe("ES6 module importing ES5 module with * as", function() {
+    describe("import * as es5star from \"../lib/module-es5\";", () => {
 
-        it("imports ES5 module and checks exported function", function() {
+        it("checks exported function", () => {
             assert.equal(es5star.testFunction(), "testFunction");
         });
 
-        it("imports ES5 module and checks exported var", function() {
+        it("checks exported var", () => {
             assert.equal(es5star.testVar, "testVar");
         });
 
-        it("imports ES5 module and checks exported object", function() {
+        it("checks exported object", () => {
             assert.equal(es5star.testObject.testProp, "testProp");
             assert.equal(es5star.testObject.testMethod(), "testMethod");
         });
 
     });
 
-    describe("ES6 module importing ES5 module with destructuring", function() {
+    describe("import { testFunction, testVar, testObject } from \"../lib/module-es5\";", () => {
 
-        it("imports ES5 module and checks exported function", function() {
+        it("checks exported function", () => {
             assert.equal(testFunction(), "testFunction");
         });
 
-        it("imports ES5 module and checks exported var", function() {
+        it("checks exported var", () => {
             assert.equal(testVar, "testVar");
         });
 
-        it("imports ES5 module and checks exported object", function() {
+        it("checks exported object", () => {
             assert.equal(testObject.testProp, "testProp");
             assert.equal(testObject.testMethod(), "testMethod");
         });
@@ -63,15 +62,39 @@ describe("ES6 module importing ES5 module with exports. syntax", function() {
     });
 
 });
-describe("ES6 module importing ES5 module with module.exports = syntax", function() {
+describe("ES6 module importing ES5 module with \"module.exports =\" syntax", () => {
 
-    describe("ES6 module importing ES5 module with destructuring", function() {
+    describe("import es5exports from \"../lib/module-es5-exports\";", () => {
 
-        it("imports ES5 module and checks exported prop", function() {
+        it("checks exported prop", () => {
+            assert.equal(es5exports.testProp, "testProp");
+        });
+
+        it("checks exported method", () => {
+            assert.equal(es5exports.testMethod(), "testMethod");
+        });
+
+    });
+
+    describe("import * as es5starExports from \"../lib/module-es5-exports\";", () => {
+
+        it("checks exported prop", () => {
+            assert.equal(es5starExports.testProp, "testProp");
+        });
+
+        it("checks exported method", () => {
+            assert.equal(es5starExports.testMethod(), "testMethod");
+        });
+
+    });
+
+    describe("import { testProp, testMethod } from \"../lib/module-es5-exports\";", () => {
+
+        it("checks exported prop", () => {
             assert.equal(testProp, "testProp");
         });
 
-        it("imports ES5 module and checks exported method", function() {
+        it("checks exported method", () => {
             assert.equal(testMethod(), "testMethod");
         });
 
